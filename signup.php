7011af66aebe21bfc778a7b7278287b76c8fd724
gtg3vv <gtg3vv@virginia.edu>
2016-10-08 12:27:10 -0400

    Added server connect

--- signup.php
+++ signup.php
@@ -4,7 +4,10 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-
+<?php
+	$db = mysqli_connect('localhost','root','demo','userData')
+	or die("Could not reach server")
+?>
 <html>
 	<head>
 		<title>Sign Up - Nuntius</title>
